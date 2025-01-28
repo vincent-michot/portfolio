0cacb7b84d46310f6149047e5c70e38131dd5891
vincent_michot <vincent_michot@carrefour.com>
2025-01-28 13:47:48 +0100

    correct the imports

---
+++ data/index.tsx
@@ -0,0 +1,422 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Experience", link: "#experience" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Cloud Big Data Solution expertise",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "./b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for data.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "./grid.svg",
+    spareImg: "./b4.svg",
+  },
+
+  {
+    id: 5,
+    title:
+      "Currently working as a data engineer at Carrefour Belgium Data Team",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-120 w-80 opacity-80",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "./b5.svg",
+    spareImg: "./grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to engage a collaboration?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "3D Solar System Planets to Explore",
+    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+    img: "./p1.svg",
+    iconLists: [
+      "./re.svg",
+      "./tail.svg",
+      "./ts.svg",
+      "./three.svg",
+      "./fm.svg",
+    ],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Yoom - Video Conferencing App",
+    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    img: "./p2.svg",
+    iconLists: [
+      "./next.svg",
+      "./tail.svg",
+      "./ts.svg",
+      "./stream.svg",
+      "./c.svg",
+    ],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "AI Image SaaS - Canva Application",
+    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    img: "./p3.svg",
+    iconLists: ["./re.svg", "./tail.svg", "./ts.svg", "./three.svg", "./c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "./p4.svg",
+    iconLists: [
+      "./next.svg",
+      "./tail.svg",
+      "./ts.svg",
+      "./three.svg",
+      "./gsap.svg",
+    ],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "./cloud.svg",
+    nameImg: "./cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "./app.svg",
+    nameImg: "./appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "./host.svg",
+    nameImg: "./hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "./s.svg",
+    nameImg: "./streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "./dock.svg",
+    nameImg: "./dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Data Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "./exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev - JSM Tech",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "./exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "./exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "./exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "./link.svg",
+  },
+];
+const currentDate = new Date().toISOString().split("T")[0];
+export const cards = [
+  {
+    description: "Orange Business",
+    title: "Data Engineer (Ops)",
+    src: "./ob.png",
+    experienceStartDate: "2024-09-01",
+    experienceEndDate: currentDate,
+    stack: [
+      { name: "GCP", rating: 4 },
+      { name: "Python", rating: 4 },
+      { name: "SQL", rating: 5 },
+      { name: "Bigquery", rating: 4 },
+      { name: "Terraform", rating: 3 },
+    ],
+    ctaText: "Details",
+    ctaLink: "https://ui.aceternity.com/templates",
+    content: () => {
+      return (
+        <ul className="w-full space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
+          <li style={{ textIndent: "-1em", paddingLeft: "1.5em" }}>
+            Developing <span className="text-purple font-semibold">GCP</span>{" "}
+            cloud based data platform
+          </li>
+          <li style={{ textIndent: "-1em", paddingLeft: "1.5em" }}>
+            Designing data warehouse, lake & marts pipelines
+          </li>
+          <li style={{ textIndent: "-1em", paddingLeft: "1.5em" }}>
+            Designing analytical and operational data models to meet business
+            needs
+          </li>
+          <li style={{ textIndent: "-1em", paddingLeft: "1.5em" }}>
+            Developing and maintaining{" "}
+            <span className="text-purple font-semibold">scalable</span> data
+            architectures (
+            <span className="text-purple font-semibold">Terraform</span>,{" "}
+            <span className="text-purple font-semibold">CI/CD</span>)
+          </li>
+          <li style={{ textIndent: "-1em", paddingLeft: "1.5em" }}>
+            Implementing data governance practices to ensure data quality,
+            security, and compliance
+          </li>
+          <li style={{ textIndent: "-1em", paddingLeft: "1.5em" }}>
+            Developing{" "}
+            <span className="text-purple font-semibold">BigData</span>{" "}
+            optimization & finOps solutions
+          </li>
+          <li style={{ textIndent: "-1em", paddingLeft: "1.5em" }}>
+            Applying <span className="text-purple font-semibold">DataOps</span>{" "}
+            methodologies to accelerate data delivery
+          </li>
+        </ul>
+      );
+    },
+  },
+  {
+    description: "Golden Palace",
+    title: "Azure Data Engineer",
+    src: "./gp.png",
+    experienceStartDate: "2023-04",
+    experienceEndDate: "2024-09",
+    stack: [
+      { name: "Databricks", rating: 4 },
+      { name: "Python", rating: 4 },
+      { name: "SQL", rating: 5 },
+      { name: "Azure", rating: 4 },
+      { name: "PowerBI", rating: 3 },
+      { name: "Network", rating: 3 },
+    ],
+    ctaText: "Details",
+    ctaLink: "https://ui.aceternity.com/templates",
+    content: () => {
+      return (
+        <ul className="w-full space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
+          <li style={{ textIndent: "-1em", paddingLeft: "1.5em" }}>
+            Creating a cloud based{" "}
+            <span className="text-purple font-semibold">
+              Modern Data Platform
+            </span>
+          </li>
+          <li style={{ textIndent: "-1em", paddingLeft: "1.5em" }}>
+            Developing a{" "}
+            <span className="text-purple font-semibold">Data Lakehouse</span>{" "}
+            using <span className="text-purple font-semibold">Azure</span> cloud
+            tools (<span className="text-purple font-semibold">Databricks</span>
+            , <span className="text-purple font-semibold">DataFactory</span>,
+            Storage Account)
+          </li>
+          <li style={{ textIndent: "-1em", paddingLeft: "1.5em" }}>
+            Designing performant data models to enable Data Analytics and BI
+          </li>
+          <li style={{ textIndent: "-1em", paddingLeft: "1.5em" }}>
+            Owning and maintaining ELT data pipelines
+          </li>
+          <li style={{ textIndent: "-1em", paddingLeft: "1.5em" }}>
+            Managing computing frameworks (
+            <span className="text-purple font-semibold">Python spark</span>, SQL
+            Warehouse, Databricks clusters)
+          </li>
+          <li style={{ textIndent: "-1em", paddingLeft: "1.5em" }}>
+            Enabling both analytics and operational applications (CRM tool,
+            PowerBI)
+          </li>
+        </ul>
+      );
+    },
+  },
+
+  {
+    description: "Isalys",
+    title: "Data Engineer",
+    src: "./isalys.png",
+    experienceStartDate: "2022-02",
+    experienceEndDate: "2023-04",
+    stack: [
+      { name: "SQL", rating: 4 },
+      { name: "Python", rating: 4 },
+      { name: "Docker", rating: 3 },
+      { name: "Kubernetes", rating: 3 },
+      { name: "Kafka", rating: 3 },
+      { name: "AWS", rating: 3 },
+      { name: "Snowflake", rating: 4 },
+    ],
+    ctaText: "Details",
+    ctaLink: "https://ui.aceternity.com/templates",
+    content: () => {
+      return (
+        <ul className="w-full space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
+          <li style={{ textIndent: "-1em", paddingLeft: "1.5em" }}>
+            Implementing on premise data pipelines
+          </li>
+          <li style={{ textIndent: "-1em", paddingLeft: "1.5em" }}>
+            Automating the creation and data loading of a data warehouse
+          </li>
+          <li style={{ textIndent: "-1em", paddingLeft: "1.5em" }}>
+            Deploying cloud solutions (AWS, snowflake)
+          </li>
+          <li style={{ textIndent: "-1em", paddingLeft: "1.5em" }}>
+            Developing and deploying modularized components (
+            <span className="text-purple font-semibold">Kubernetes</span>,{" "}
+            <span className="text-purple font-semibold">Docker</span>)
+          </li>
+          <li style={{ textIndent: "-1em", paddingLeft: "1.5em" }}>
+            Creating a monitoring/alerting tool to keep track of data flows
+          </li>
+          <li style={{ textIndent: "-1em", paddingLeft: "1.5em" }}>
+            Developing real time (time series) data solutions (
+            <span className="text-purple font-semibold">KAFKA</span>,{" "}
+            <span className="text-purple font-semibold">Redshift</span>)
+          </li>
+          <li style={{ textIndent: "-1em", paddingLeft: "1.5em" }}>
+            Enabling real time analytics (Tableau, Graphana)
+          </li>
+        </ul>
+      );
+    },
+  },
+  {
+    description: "Safran Aircraft Engines",
+    title: "Data Scientist (internship)",
+    src: "./safran.png",
+    experienceStartDate: "2021-04",
+    experienceEndDate: "2021-10",
+    stack: [
+      { name: "Python", rating: 4 },
+      { name: "SQL", rating: 5 },
+      { name: "Tensorflow", rating: 3 },
+      { name: "Pandas", rating: 4 },
+      { name: "IA", rating: 3 },
+    ],
+    ctaText: "Details",
+    ctaLink: "https://ui.aceternity.com/templates",
+    content: () => {
+      return (
+        <ul className="w-full space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
+          <li style={{ textIndent: "-1em", paddingLeft: "1.5em" }}>
+            R&D engineer in{" "}
+            <span className="text-purple font-semibold">data science</span>,
+            artificial intelligence developer
+          </li>
+          <li style={{ textIndent: "-1em", paddingLeft: "1.5em" }}>
+            Developing end to end{" "}
+            <span className="text-purple font-semibold">Deep Learning</span>{" "}
+            solution (automatic detection of surface defects on aricraft turbo
+            reactors, Image Processing)
+          </li>
+          <li style={{ textIndent: "-1em", paddingLeft: "1.5em" }}>
+            Deploying, communicating and outreaching of the new technology
+          </li>
+        </ul>
+      );
+    },
+  },
+];
